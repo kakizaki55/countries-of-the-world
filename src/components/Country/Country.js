fc6e5fd1bebc9271b10e3de2ed82c1783eaf74d2
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-07 09:03:29 -0800

    got the images to render

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './Country.css';
 
 export default function CountryCard({ name, iso2, continent }) {
-  console.log(name);
   return (
     <>
       <div className="name">{name}</div>
       <div className="iso2">{iso2}</div>
       <div className="continent">{continent}</div>
-      <img src="https://flagcdn.com/32x24/za.png" />
+      <img src={`https://flagcdn.com/32x24/${iso2.toLowerCase()}.png`} />
     </>
   );
 }
