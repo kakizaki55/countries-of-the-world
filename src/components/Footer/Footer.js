120f79ddc611ede28c556cf74e25a31296b1c7ef
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-07 17:08:55 -0800

    tring to fix npm test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div>
-      <h1> The Countries Around the World</h1>
+      <h1> made by Minoka </h1>
     </div>
   );
 }
